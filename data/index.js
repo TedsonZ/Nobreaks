ad5470d78faa8ab750bc12a0c8760061359f2775
TedsonZ <60135791+TedsonZ@users.noreply.github.com>
2021-09-14 15:43:36 -0300

    Update index.js

--- data/index.js
+++ data/index.js
@@ -8,6 +8,9 @@
 
 var gateway = `ws://${window.location.hostname}/ws`;
 var websocket;
+var alarmeAc = 0;
+var alarmeLow = 0;
+var play = 0;
 
 // ----------------------------------------------------------------------------
 // Initialization
@@ -27,13 +30,14 @@ function onLoad(event) {
 function initWebSocket() {
     console.log('Trying to open a WebSocket connection...');
     websocket = new WebSocket(gateway);
-    websocket.onopen    = onOpen;
-    websocket.onclose   = onClose;
+    websocket.onopen = onOpen;
+    websocket.onclose = onClose;
     websocket.onmessage = onMessage;
 }
 
 function onOpen(event) {
     console.log('Connection opened');
+    ler();
 }
 
 function onClose(event) {
@@ -50,12 +54,12 @@ function onMessage(event) {
     let data = JSON.parse(event.data);
     console.log(data);
     //document.getElementById('led_' + data.rank).className = data.status;
-    
+
     document.getElementById('localizacao').innerText = data.local;
     document.getElementById('acfail').innerText = data.acfail;
     document.getElementById('lowbat').innerText = data.lowbat;
 
-    
+
     document.getElementById('ld0').innerText = data.log.ld0;
     document.getElementById('lh0').innerText = data.log.lh0;
     document.getElementById('lm0').innerText = data.log.lm0;
@@ -75,29 +79,36 @@ function onMessage(event) {
     document.getElementById('ld4').innerText = data.log.ld4;
     document.getElementById('lh4').innerText = data.log.lh4;
     document.getElementById('lm4').innerText = data.log.lm4;
-    
-    if(data.acfail === "Normal"){        
-    document.getElementById('acfail').style.color = 'black';
-    document.getElementById("bt_ac").style.display = "none";
-    } else if(data.acfail === "Falha") {        
-    document.getElementById('acfail').style.color = 'red';  
-    document.getElementById("bt_ac").style.display = "block";  
-    }else{
-    document.getElementById('acfail').style.color = 'red';
-    document.getElementById("bt_ac").style.display = "none";
+
+    if (data.acfail === "Normal") {
+        document.getElementById('acfail').style.color = 'black';
+        document.getElementById("bt_ac").style.display = "none";
+        alarmeAc = 0;
+    } else if (data.acfail === "Falha") {
+        document.getElementById('acfail').style.color = 'red';
+        document.getElementById("bt_ac").style.display = "block";
+        alarmeAc = 1;
+    } else {
+        document.getElementById('acfail').style.color = 'red';
+        document.getElementById("bt_ac").style.display = "none";
+        alarmeAc = 0;
     }
 
-    if(data.lowbat === "Normal"){        
-    document.getElementById('lowbat').style.color = 'black';
-    document.getElementById("bt_low").style.display = "none";
-    }else if(data.lowbat === "Falha") {        
-    document.getElementById('lowbat').style.color = 'red';  
-    document.getElementById("bt_low").style.display = "block";  
-    }else{
-    document.getElementById('lowbat').style.color = 'red';
-    document.getElementById("bt_low").style.display = "none";
-    } 
-    
+    if (data.lowbat === "Normal") {
+        document.getElementById('lowbat').style.color = 'black';
+        document.getElementById("bt_low").style.display = "none";
+        alarmeLow = 0;
+    } else if (data.lowbat === "Falha") {
+        document.getElementById('lowbat').style.color = 'red';
+        document.getElementById("bt_low").style.display = "block";
+        alarmeLow = 1;
+    } else {
+        document.getElementById('lowbat').style.color = 'red';
+        document.getElementById("bt_low").style.display = "none";
+        alarmeLow = 0;
+    }
+console.log(alarmeAc);
+console.log(alarmeLow);
 }
 // ----------------------------------------------------------------------------
 // Button handling
@@ -109,44 +120,53 @@ function initButton() {
 }
 
 function clickacfail(event) {
-    websocket.send(JSON.stringify({'action':'reconhecerAcfail'}));
+    websocket.send(JSON.stringify({ 'action': 'reconhecerAcfail' }));
 }
 function clickLowbat(event) {
-    websocket.send(JSON.stringify({'action':'reconhecerLowbat'}));
+    websocket.send(JSON.stringify({ 'action': 'reconhecerLowbat' }));
 }
 
 //-----------------------------------------------------------------------------
 //SOM
 //-----------------------------------------------------------------------------
 let context = new AudioContext(),
-                oscillator = context.createOscillator();
+    oscillator = context.createOscillator();
+let i = 1;
 
-            let i = 1;
-
-            function tocar(){
-                if(tocar === 1){
-            setInterval(function run() {
-                let resto = i % 2;
-                console.log(resto);
-                if (resto) {
-                    context = new AudioContext();
-                    oscillator = context.createOscillator();
-                    oscillator.type = "sine";
-                    oscillator.connect(context.destination);
-                    oscillator.start();
-                    console.log("Play");
-                } else {
-                    oscillator.stop(context.currentTime + 0);
-                    console.log("Pause");
-                }
-                i++
-            }, 1000);};};
+setInterval(function run() {
+    if (alarmeAc === 1 || alarmeLow === 1) {
+        let resto = i % 2;
+        console.log(resto);
+        if (resto) {
+            context = new AudioContext();
+            oscillator = context.createOscillator();
+            oscillator.type = "sine";
+            oscillator.connect(context.destination);
+            oscillator.start();
+            play = 1;
+            console.log("Play");
+        } else {
+            oscillator.stop(context.currentTime + 0);
+            console.log("Pause");
+        }
+        i++
+    } else {
+        if (play === 1) {
+            oscillator.stop(context.currentTime + 0);
+            play = 0;
+        }
+    }
+}, 1000);
 
 setTimeout(() => {
-    websocket.send(JSON.stringify({'action':'obterleitura'}));
+    websocket.send(JSON.stringify({ 'action': 'obterleitura' }));
     datacao();
     //ocultar();
 }, 2000);
+
+function ler() {
+    websocket.send(JSON.stringify({ 'action': 'obterleitura' }));
+}
 //let lowBat = document.getElementById('lowbat');
 ////document.getElementById('bt_ac').style.display = 'none';
 //document.getElementById('bt_low').style.display = 'inline';
@@ -156,10 +176,11 @@ function ocultar() {
     document.getElementById("bt_low").style.display = "none";
 }
 
-function datacao(){
+function datacao() {
     now = new Date
     let dia = now.getDate() + "/" + now.getMonth() + "/" + now.getFullYear();
     let hora = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
     console.log(dia);
     console.log(hora);
 }
+
